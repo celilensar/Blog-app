d4cfb155e57ca3c5fd17e0b7e2b63ed6f4de2530
celilensar <ensar_avc@outlook.com>
2024-01-29 21:22:30 +0300

    Login Page form added

--- frontend/src/components/Auth.js
+++ frontend/src/components/Auth.js
@@ -1,9 +1,45 @@
-import React from 'react'
+import { Box, Button, TextField, Typography } from "@mui/material";
+import React from "react";
 
 const Auth = () => {
   return (
-	<div>Auth</div>
-  )
-}
+    <div>
+      <form>
+        <Box
+          maxWidth={400}
+          display="flex"
+          flexDirection="column"
+          alignItems={"center"}
+          justifyContent={"center"}
+          boxShadow={"10px 10px 20px #ccc"}
+          padding={3}
+          margin="auto"
+          marginTop={5}
+          borderRadius={10}
+          paddingY={5}
+        >
+          <Typography variant="h2" padding={3} textAlign={"center"}>
+            Login
+          </Typography>
+          <TextField placeholder="Name" margin="normal" />
+          <TextField type="email" placeholder="Email" margin="normal" />
+          <TextField type="password" placeholder="Password" margin="normal" />
+          <Button
+            variant="contained"
+            sx={{ borderRadius: 3, marginTop: 3 }}
+            color="warning"
+          >
+            {" "}
+            Submit
+          </Button>
+          <Button sx={{ borderRadius: 3, marginTop: 3 }}>
+            {" "}
+            Change to Signup
+          </Button>
+        </Box>
+      </form>
+    </div>
+  );
+};
 
-export default Auth
+export default Auth;
